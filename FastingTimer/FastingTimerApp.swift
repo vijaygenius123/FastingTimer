591b1114ae33897a784d44923b63fdd87bb0d902
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-09-12 06:04:02 +0100

    Initial Commit

---
+++ FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Vijayaraghavan Sundararaman on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
